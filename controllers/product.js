e6d30014c7f8201a0e99a623276b3054e35e924b
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-05-24 22:56:41 +0100

    validate create product data

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,8 @@
 const Product = require('./../models/product');
 const formidable = require('formidable');
 const fs = require('fs');
+const Joi = require('joi');
+const { join } = require('path');
 
 exports.createProduct = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -9,7 +11,21 @@ exports.createProduct = (req, res) => {
     form.parse(req, (err, fields, files) => {
         if(err) return res.status(400).json({error: "Upload faild!"})
         let product = new Product(fields);
+
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.required(),
+            quantity: Joi.required(),
+            category: Joi.string().required(),
+        });
+
+        const { error } = schema.validate();
+        if(error)
+            return res.status(400).json({error: error.details[0].message}) 
         if(files.photo){
+            if(files.photo.size > Math.pow(10, 6)) 
+                return res.status(400).json({message: "Image should be less than 1mb!"}) 
             product.photo.data = fs.readFileSync(files.photo.filepath);
             product.photo.contentType = product.photo.type;
         }
