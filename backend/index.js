17de6a80b51c1439fdf4de0bd4e21589eee66971
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-05-25 14:07:39 +0100

    add cors & refactor project structure

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,17 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
+const cors = require('cors');
+const authRoutes = require('./src/routes/auth');
+const userRoutes = require('./src/routes/user');
+const categoryRoutes = require('./src/routes/category');
+const productRoutes = require('./src/routes/product');
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(expressValidator());
 
